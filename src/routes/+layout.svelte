d7b7e68d09736df550f35d9b8029c907e750f9cd
João Ramos <1joaoramos1@gmail.com>
2026-02-15 10:13:45 +0100

    Creates nav bar place holder pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
-
-	let { children } = $props();
+	import Nav from '$lib/components/Nav.svelte';
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
-{@render children()}
+<svelte:head>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+<Nav />
+
+<main>
+	<slot />
+</main>
---
+++ src/routes/qis-handbook/+page.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<h1>QIS Handbook</h1>
